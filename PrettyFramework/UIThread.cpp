e331bf1fbf9ff366c5adbb2bead0dc119571f0b0
lvan100@yeah.net <lvan100@yeah.net>
2015-12-31 10:06:49 +0800

    完善界面线程；

--- PrettyFramework/UIThread.cpp
+++ PrettyFramework/UIThread.cpp
@@ -13,17 +13,42 @@ namespace PrettyFramework {
 	}
 
 	UIThread::UIThread()
+		: exit_event(INVALID_HANDLE_VALUE)
+		, thread(INVALID_HANDLE_VALUE)
+		, will_exit(false)
+		, thread_id(0)
 	{}
 	
 	UIThread::~UIThread()
 	{}
 
+	void UIThread::AddWindow(void* window)
+	{
+		windows.insert(make_pair(window, false));
+	}
+	
+	void UIThread::RemoveWindow(void* window)
+	{
+		auto& iter = windows.find(window);
+		if (iter != windows.end()) {
+			windows.erase(iter);
+		}
+	}
+
+	void UIThread::UpdateWindow(void* window)
+	{
+		auto& iter = windows.find(window);
+		if (iter != windows.end()) {
+			(*iter).second = true;
+		}
+	}
+
 	/**
 	 * 界面线程执行函数入口
 	 */
-	static DWORD WINAPI UIThreadEntry(LPVOID pParam)
+	static DWORD WINAPI UIThreadEntry(LPVOID param)
 	{
-		UIThread* ui = (UIThread*)pParam;
+		UIThread* ui = (UIThread*)param;
 		if (ui != nullptr) {
 			return ui->Run();
 		}
@@ -32,19 +57,71 @@ namespace PrettyFramework {
 
 	void UIThread::Start()
 	{
-		DWORD nThreadId;
-		HANDLE handle = CreateThread(NULL, 0, UIThreadEntry, (LPVOID)this, CREATE_SUSPENDED, &nThreadId);
-		SetThreadPriority(handle, THREAD_PRIORITY_ABOVE_NORMAL);
-		ResumeThread(handle);
+		if (thread != INVALID_HANDLE_VALUE) {
+			DWORD dwResult = WaitForSingleObject(thread, 0);
+			if (WAIT_TIMEOUT == dwResult) {
+				return; // 运行中
+			}
+		}
+
+		if (exit_event == INVALID_HANDLE_VALUE) {
+			exit_event = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+		}
+
+		// 创建不直接启动的线程
+		thread = CreateThread(NULL, 0, UIThreadEntry, (LPVOID)this
+			, CREATE_SUSPENDED, &thread_id);
+
+		// 将界面线程的优先级调高
+		SetThreadPriority(thread, THREAD_PRIORITY_ABOVE_NORMAL);
+
+		// 重置退出事件的通知信号
+		ResetEvent(exit_event);
+		will_exit = false;
+
+		// 重新启动线程
+		ResumeThread(thread);
 	}
 
-	void UIThread::Stop()
+	void UIThread::Stop(int timeout)
 	{
+		will_exit = true; /* 即将退出 */
 
+		DWORD dwResult = WaitForSingleObject(exit_event, timeout);
+		if (WAIT_OBJECT_0 != dwResult) { /* 未能成功退出 */
+			TerminateThread(thread, 0);
+		}
+
+		thread = INVALID_HANDLE_VALUE;
+		thread_id = 0;
+
+		ResetEvent(exit_event);
+		will_exit = false;
 	}
 
 	int UIThread::Run()
 	{
+		while (!will_exit) {
+
+			DWORD dwTick = GetTickCount();
+
+			for (auto& iter = windows.begin();
+				 iter != windows.end();
+				 iter++)
+			{
+				if ((*iter).second) {
+					::RedrawWindow((HWND)(*iter).first, nullptr, nullptr
+						, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ERASE);
+				}
+			}
+
+			DWORD dwRedraw = GetTickCount() - dwTick;
+			Sleep(16 - dwRedraw % 16); /* 等待 */
+		}
+
+		// 通知线程成功退出
+		SetEvent(exit_event);
+
 		return 0;
 	}
 
--- PrettyFramework/UIThread.h
+++ PrettyFramework/UIThread.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <map>
+using namespace std;
+
 namespace PrettyFramework {
 
 	/**
@@ -12,14 +15,36 @@ namespace PrettyFramework {
 		~UIThread();
 
 		/**
+		 * 添加本地窗口
+		 */
+		void AddWindow(void* window);
+
+		/**
+		 * 移除本地窗口
+		 */
+		void RemoveWindow(void* window);
+
+		/**
+		 * 刷新窗口
+		 */
+		void UpdateWindow(void* window);
+
+	private:
+		/**
+		 * 刷新窗口列表
+		 */
+		map<void*, bool> windows;
+
+	public:
+		/**
 		 * 启动线程
 		 */
 		void Start();
 
 		/**
-		 * 停止线程
+		 * 停止线程，若超时将强制杀死
 		 */
-		void Stop();
+		void Stop(int timeout = INFINITE);
 
 		/**
 		 * 线程执行
@@ -28,9 +53,24 @@ namespace PrettyFramework {
 
 	private:
 		/**
-		 * 本地线程
+		 * 本地线程句柄
 		 */
 		HANDLE thread;
+
+		/**
+		 * 本地线程ID号
+		 */
+		DWORD thread_id;
+
+		/**
+		 * 即将结束线程，用于通知线程必须退出
+		 */
+		volatile bool will_exit;
+
+		/**
+		 * 线程退出事件，用于通知线程已经退出
+		 */
+		HANDLE exit_event;
 	};
 	
 	/**
